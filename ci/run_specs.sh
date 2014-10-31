57d8566ae05abe07be8a3abc5bf6889e59e49f7e
Aram Price and Rob Day-Reynolds <pair+aram+rdayreynolds@pivotal.io>
2014-10-31 10:30:36 -0700

    `--env=X=y`, not `--env X=y`

--- ci/run_specs.sh
+++ ci/run_specs.sh
@@ -8,9 +8,9 @@ docker run \
   --volume=${PWD}:/vsphere_clients \
   --workdir=/vsphere_clients \
   ${DOCKER_REGISTRY_HOST}/${DOCKER_IMAGE_NAME} \
-  --env VCENTER_IP=${VCENTER_IP} \
-  --env USERNAME=${USERNAME} \
-  --env PASSWORD=${PASSWORD} \
-  --env DATACENTER_NAME=${DATACENTER_NAME} \
-  --env DATASTORE_NAME=${DATASTORE_NAME} \
+  --env=VCENTER_IP=${VCENTER_IP} \
+  --env=USERNAME=${USERNAME} \
+  --env=PASSWORD=${PASSWORD} \
+  --env=DATACENTER_NAME=${DATACENTER_NAME} \
+  --env=DATASTORE_NAME=${DATASTORE_NAME} \
   /bin/sh -c 'bundle && bundle exec rspec --format documentation'
