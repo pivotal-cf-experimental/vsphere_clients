6df004fb06fba78e3d97a5d31c52735c6ec2e64d
Aram Price and Rob Day-Reynolds <pair+aram+rdayreynolds@pivotal.io>
2014-10-31 10:59:38 -0700

    Swap argument order

--- ci/run_specs.sh
+++ ci/run_specs.sh
@@ -7,10 +7,10 @@ docker run \
   --rm=true \
   --volume=${PWD}:/vsphere_clients \
   --workdir=/vsphere_clients \
-  ${DOCKER_REGISTRY_HOST}/${DOCKER_IMAGE_NAME} \
   --env=VCENTER_IP=${VCENTER_IP} \
   --env=USERNAME=${USERNAME} \
   --env=PASSWORD=${PASSWORD} \
   --env=DATACENTER_NAME=${DATACENTER_NAME} \
   --env=DATASTORE_NAME=${DATASTORE_NAME} \
+  ${DOCKER_REGISTRY_HOST}/${DOCKER_IMAGE_NAME} \
   /bin/sh -c 'bundle && bundle exec rspec --format documentation'
